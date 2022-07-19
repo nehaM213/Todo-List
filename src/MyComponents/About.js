ec2fcef8f4030e3cc39ac50cba465d604be2da16
Neha Maurya <nehamaurya213@gmail.com>
2022-07-20 03:06:09 +0530

    Final Project upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h3>This is about page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima velit nulla, minus fugit enim placeat sed voluptatibus officia impedit vel distinctio quo eaque optio? Fugiat architecto quisquam nihil veritatis odio.</p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//import "./footer.css"
+
+export const Footer = () => {
+  // let footerStyle={
+  //   position:"relative",
+  //   top:"10vh",
+  //   //relative to % of viewport height
+  //   width:"100%",
+  // }
+  return (
+    <footer className='bg-dark text-light' /**style={footerStyle}**/>
+      <p className='text-center'>Copyright &copy;MyTodosList</p>
+      </footer>
+  )
+}
